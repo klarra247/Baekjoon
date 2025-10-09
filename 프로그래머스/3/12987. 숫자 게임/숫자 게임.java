8b12c831172e70ee27e3b86d447d9c44a85e9d1b
Klarra (Sihyeon) Lee <101632525+klarra247@users.noreply.github.com>
2025-10-09 14:37:20 +0900

    [level 3] Title: 숫자 게임, Time: 138.94 ms, Memory: 67.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int a = 0;
+        int b = 0;
+        
+        while (a < A.length && b < B.length) {
+            if (A[a] > B[b]) {
+                b++;
+            } else if (A[a] < B[b]){
+                answer++;
+                a++;
+                b++;
+            } else {
+                b++;
+            }
+        }
+        
+        return answer;
+        
+    }
+}
